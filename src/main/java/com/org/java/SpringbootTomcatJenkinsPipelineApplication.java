12f63d47e2c3d958a0f709a35c3f672f0eaaa533
thrishankgit99 <thrishankgithub@gmail.com>
2024-12-10 15:38:21 +0530

    fst cmmited

---
+++ src/main/java/com/org/java/SpringbootTomcatJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTomcatJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTomcatJenkinsPipelineApplication.class, args);
+	}
+
+}
